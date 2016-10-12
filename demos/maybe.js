d5c2f32671805b3bf8e8ce3f122053b113e0e4f1
ShMcK <shawn.j.mckay@gmail.com>
2016-10-11 22:22:45 -0700

    use jest for tests

--- demos/maybe.js
+++ demos/maybe.js
@@ -3,33 +3,35 @@
 const M = require('ramda-fantasy').Maybe
 const R = require('ramda')
 
-console.log('Maybe')
+describe('Maybe:', () => {
 
-// update a value
-console.log('1', 
-  M.of(0)
-    .map(R.inc)
-)
-          
-// update an array
-console.log('2',
-  M.of([1, 2, 3])
-    .map(R.map(R.inc))
-)
+  it('update a value', () => {
+    const r = M.of(0)
+        .map(R.inc)
+    expect(r).toEqual( M.Just(1) )
+  })
 
-// update a nested object
+  it('update an array', () => {
+    const r = M.of([1, 2, 3])
+        .map(R.map(R.inc))
+    expect(r).toEqual( M.Just([2, 3, 4]) )
+  })
 
-const a ={ a: { b: 1 } }
-const bLens = R.lensPath(['a', 'b'])
-const cLens = R.lensPath(['a', 'c'])
-console.log('3',
-  M.of(a)
-    .map(R.set(bLens, 2))
-    .map(R.set(cLens, 2))
-)
+  it('update a nested object', () => {
+    const a = { a: { b: 1 } }
+    const bLens = R.lensPath(['a', 'b'])
+    const cLens = R.lensPath(['a', 'c'])
+    const r = M.of(a)
+        .map(R.set(bLens, 2))
+        .map(R.set(cLens, 2))
+    expect(r).toEqual( M.Just({ a: { b: 2, c: 2}}) )
+  })
 
-// catch an undefined value
-const lookup = x => x ? M.Just(x) : M.Nothing()
-console.log('4',
-  lookup(a.c)
-)
+  it('should catch an undefined value', () => {
+    const a = { a: { b: 1 } }
+    const lookup = x => x ? M.Just(x) : M.Nothing()
+    const r = lookup(a.c)
+    expect(r).toBe( M.Nothing() )
+  })
+
+})
